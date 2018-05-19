f4135a50a02f24b08bd51297dccf20fd41f66980
Reginaldo Junior <76regi@gmail.com>
2018-05-19 10:07:47 -0300

    Doing method to implements calc IRFF and INSS discounts yearly

--- app/Service/CLT.php
+++ app/Service/CLT.php
@@ -8,5 +8,54 @@ namespace App\Service;
 
 class CLT extends ContractAbstract
 {
+
+    const VALUE_BASEAD_INSS = 171.77;
+    const FACTOR_INSS = 13;
+    const FACTOR_MULTIPLICATE_IRFF = 0.2;
+    const VALUE_MIN_YEARLY_TO_IRFF = 9400;
     
+    /**
+     * Get One Third Vacacy
+     * With remuneration current divid by three
+     *
+     * @return float
+     */
+    public function getOneThirdVacacy()
+    {
+        if ($this->hasVacacy) {
+            $oneThirdVacacy = ($this->remunerationByMonth / 3);
+            
+            return number_format($oneThirdVacacy, 2);
+        }
+
+        return 0.00;
+    }
+
+    public function getINSSYearlyByRemuneration()
+    {
+        return self::FACTOR_INSS * self::VALUE_BASEAD_INSS;
+    }
+
+    /**
+     * Rules to calc sum yearly remuneration, vacacy, PLR
+     *
+     * @return void
+     */
+    public function getIRFFYearlyByRemuneration()
+    {
+        $yearlyRemuneration = (
+            $this->sumSalaryYearlyBrute() + 
+            $this->sumValueVacacy() + 
+            $this->getValueMonthProfitSharing()
+        );
+
+        $yearlyRemunerationFirstRule = $yearlyRemuneration * self::VALUE_BASEAD_INSS;
+
+        if ($yearlyRemunerationFirstRule <= self::VALUE_MIN_YEARLY_TO_IRFF) {
+            return 1163.77;
+        }
+        
+        return 1163.77;
+    }
+
 }
--- tests/Unit/Service/CLTTest.php
+++ tests/Unit/Service/CLTTest.php
@@ -33,4 +33,47 @@ class CLTTest extends TestCase
         $this->assertEquals(2300, $this->CLT->sumValueVacacy());
     }
 
+    public function testGetOneThirdVacacy()
+    {
+        $this->CLT->setRemunerationBymonth(2300);
+
+        $this->assertEquals(766.67, $this->CLT->getOneThirdVacacy());
+    }
+
+    public function testGetValueMonthProfitSharing()
+    {
+        $this->CLT->setPercentageByRemunerationProfitSharing(10);
+        $this->CLT->setRemunerationByMonth(5000);
+
+        $profitSharing = $this->CLT->getValueMonthProfitSharing();
+
+        $this->assertEquals(500, $profitSharing);
+    }
+
+    public function testGetValueMonthProfitSharingWithDecimalNumber()
+    {
+        $this->CLT->setPercentageByRemunerationProfitSharing(1.5);
+        $this->CLT->setRemunerationByMonth(5000);
+
+        $profitSharing = $this->CLT->getValueMonthProfitSharing();
+
+        $this->assertEquals(75, $profitSharing);
+    }
+
+    // Descontos anuais	R$ 3,488.78
+    // INSS (salário, 13o. e férias)	R$ 2,233.01
+    // IRPF (Simples - Dedução 20% ou R$9.400)	R$ 1,163.77
+    // Vale refeição (anual) - Dedução 20%	R$ 12.00
+    // Outros descontos anuais (sindical etc.)	R$ 80.00    
+    public function testDiscountYearlyBaseadOnRemunerationAndBenefities()
+    {
+        $this->CLT->setRemunerationByMonth(2300);
+        
+        $inss = $this->CLT->getINSSYearlyByRemuneration();
+        $irff = $this->CLT->getIRFFYearlyByRemuneration();
+
+        $this->assertEquals(2233.01, $inss);
+        $this->assertEquals(1163.77, $irff);
+    }
+
 }
